8b9200abe44382c957932ba2e11314fddc45127d
Andrew Nkhata <andrew_nkhata@outlook.com>
2025-04-21 15:27:04 +0200

    school-deploy

--- src/api/apiClient.jsx
+++ src/api/apiClient.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:7223/api';
+//const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:7223/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://agileepilogue-001-site2.atempurl.com/api';
 // const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const apiClient = axios.create({
